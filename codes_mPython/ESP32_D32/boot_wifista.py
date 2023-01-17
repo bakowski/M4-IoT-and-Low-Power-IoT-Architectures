5f886cd24e1cdd8a71d8c6e0c4303436c39ad57a
Bakowski <89449454+smartcomputerlab@users.noreply.github.com>
2023-01-17 15:54:40 +0100

    Add files via upload

---
+++ codes_mPython/ESP32_D32/boot_wifista.py
@@ -0,0 +1,30 @@
+# This file is executed on every boot (including wake-boot from deep sleep)
+
+def do_connect(ssid: str, password: str):
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+
+if __name__ == '__main__':
+    # Put yor Wi-Fi credentials here
+    my_ssid = "Livebox-08B0"
+    my_pass = "G79ji6dtEptVTPWmZP"
+
+    try:
+        from max30102 import MAX30102
+    except:
+        print("'max30102' not found!")
+        try:
+            import upip
+            do_connect(my_ssid, my_pass)
+            upip.install("micropython-max30102")
+        except:
+            print("Unable to get 'micropython-max30102' package!")
+
